a366fc18965dc723bb6e9309e5f31e2a590faa3c
Ikeda <brandon_ikeda@hotmail.com>
2017-03-27 12:20:05 -0700

    Second commit day 11

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// Get our Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out Functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+// Hook up event listeners
